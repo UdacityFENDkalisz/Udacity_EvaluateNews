f36b378eccf717798cf13d7107b5d91c8975f885
Martha Kalisz <74026680+mskalisz@users.noreply.github.com>
2021-01-31 06:25:21 -0500

    Update service worker

--- evaluate-news-nlp/webpack.dev.js
+++ evaluate-news-nlp/webpack.dev.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -49,6 +50,7 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new webpack.EnvironmentPlugin(['API_KEY'])
+        new webpack.EnvironmentPlugin(['API_KEY']),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
